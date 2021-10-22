eb6f9bc286a562c0a3c439f16d0c8648c1f5c1de
mohamedmselmi <mohamed.mselmi1@esprit.tn>
2021-10-22 13:16:25 +0200

    last commit

--- src/main/java/tn/esprit/spring/entities/Employe.java
+++ src/main/java/tn/esprit/spring/entities/Employe.java
@@ -40,7 +40,7 @@ public class Employe implements Serializable {
 		
 	//@Column(unique=true)
 	//@Pattern(regex=".+\@.+\..+")
-	private String emailshhhhhhhhhhhh;
+	private String email;
 
 	private String password;
 	
